f7792662a1474bb04109585724549cccf90b6c0c
Alex <alexandria.ross@gmail.com>
2020-04-27 04:04:13 -0400

    Add files via upload

--- Final_Project.R
+++ Final_Project.R
@@ -4,13 +4,15 @@ load("C:/Users/aross4/Desktop/R Folder/obiwanaddone/data/StarWars.RData")
 #StarWars
 #View(StarWars[1:10,1:14])
 
-add_one <- function (Starwars, height) {
-  taller <- height + 1
-  return(taller)
+add_one <- function (dataset, x) {
+  t <- x + 1
+  return(t)
 }
+add_one(StarWars,StarWars$height)
 
-taller <-c(StarWars$height + 1)
-StarWars$taller <-StarWars[,2]+1
+#taller <-c(StarWars$height + 1)
+#StarWars$taller <-StarWars[,2]+1
+StarWars$taller <-add_one(StarWars,StarWars$height)
 
 StarWars$height
 StarWars$taller
